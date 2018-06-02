65ec37c3fe5694cd9dfdea88d0bc8992cb0452f8
lirugo <roostik21@gmail.com>
2018-06-02 15:38:36 +0300

    restyle users page

--- views/users/index.php
+++ views/users/index.php
@@ -22,82 +22,50 @@ $this->title = 'My Yii Application';
 
        <div class="row">
            <div class="col-md-4">
-               <div class="panel panel-primary">
+               <div class="panel panel-default">
+                   <div>
+                    <img class="panel-img-top center" src="https://www.w3schools.com/howto/img_avatar.png" width="100%" alt="Card image cap">
+                   </div>
                    <div class="panel-body">
-                       <div class="col-md-8">
-                           Login<br/>
-                           email@app.com<br/>
-                           Benjamin Franklin<br/>
-                           Male<br/>
-                           22-12-1888 12:59<br/>
-                       </div>
-                       <div class="col-md-4">
-                           <img src="https://cdn.iconscout.com/public/images/icon/free/png-512/avatar-user-hacker-3830b32ad9e0802c-512x512.png" class="pull-right" width="100px"/>
-                       </div>
+                       <h2 class="text-center">Benjamin Franklin</h2>
+                       <p class="panel-text">
+                           Login <br/>
+                           email@app.com <br/>
+                           12-12-2012 12:12 <br/>
+                       </p>
+                       <a href="#" class="btn btn-sm btn-primary pull-right">Edit profile</a>
                    </div>
                </div>
            </div>
            <div class="col-md-4">
-               <div class="panel panel-primary">
+               <div class="panel panel-default">
+                   <div>
+                    <img class="panel-img-top center" src="https://www.w3schools.com/howto/img_avatar.png" width="100%" alt="Card image cap">
+                   </div>
                    <div class="panel-body">
-                       <div class="col-md-8">
-                           Login<br/>
-                           email@app.com<br/>
-                           Benjamin Franklin<br/>
-                           Male<br/>
-                           22-12-1888 12:59<br/>
-                       </div>
-                       <div class="col-md-4">
-                           <img src="https://cdn.iconscout.com/public/images/icon/free/png-512/avatar-user-hacker-3830b32ad9e0802c-512x512.png" class="pull-right" width="100px"/>
-                       </div>
+                       <h2 class="text-center">Benjamin Franklin</h2>
+                       <p class="panel-text">
+                           Login <br/>
+                           email@app.com <br/>
+                           12-12-2012 12:12 <br/>
+                       </p>
+                       <a href="#" class="btn btn-sm btn-primary pull-right">Edit profile</a>
                    </div>
                </div>
            </div>
            <div class="col-md-4">
-               <div class="panel panel-primary">
-                   <div class="panel-body">
-                       <div class="col-md-8">
-                           Login<br/>
-                           email@app.com<br/>
-                           Benjamin Franklin<br/>
-                           Male<br/>
-                           22-12-1888 12:59<br/>
-                       </div>
-                       <div class="col-md-4">
-                           <img src="https://cdn.iconscout.com/public/images/icon/free/png-512/avatar-user-hacker-3830b32ad9e0802c-512x512.png" class="pull-right" width="100px"/>
-                       </div>
+               <div class="panel panel-default">
+                   <div>
+                    <img class="panel-img-top center" src="https://www.w3schools.com/howto/img_avatar.png" width="100%" alt="Card image cap">
                    </div>
-               </div>
-           </div>
-           <div class="col-md-4">
-               <div class="panel panel-primary">
                    <div class="panel-body">
-                       <div class="col-md-8">
-                           Login<br/>
-                           email@app.com<br/>
-                           Benjamin Franklin<br/>
-                           Male<br/>
-                           22-12-1888 12:59<br/>
-                       </div>
-                       <div class="col-md-4">
-                           <img src="https://cdn.iconscout.com/public/images/icon/free/png-512/avatar-user-hacker-3830b32ad9e0802c-512x512.png" class="pull-right" width="100px"/>
-                       </div>
-                   </div>
-               </div>
-           </div>
-           <div class="col-md-4">
-               <div class="panel panel-primary">
-                   <div class="panel-body">
-                       <div class="col-md-8">
-                           Login<br/>
-                           email@app.com<br/>
-                           Benjamin Franklin<br/>
-                           Male<br/>
-                           22-12-1888 12:59<br/>
-                       </div>
-                       <div class="col-md-4">
-                           <img src="https://cdn.iconscout.com/public/images/icon/free/png-512/avatar-user-hacker-3830b32ad9e0802c-512x512.png" class="pull-right" width="100px"/>
-                       </div>
+                       <h2 class="text-center">Benjamin Franklin</h2>
+                       <p class="panel-text">
+                           Login <br/>
+                           email@app.com <br/>
+                           12-12-2012 12:12 <br/>
+                       </p>
+                       <a href="#" class="btn btn-sm btn-primary pull-right">Edit profile</a>
                    </div>
                </div>
            </div>
